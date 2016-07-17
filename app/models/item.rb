f2cb0c3a89e888ac5584b9d1c9690a5678737b95
tyleresmith <oats000@hotmail.com>
2016-07-17 14:53:50 -0400

    corrected seed data

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belongs_to :cateogry
+  belongs_to :category
   validates :name, length: { minimum: 3 }
   validates :description, length: { minimum: 10 }
   validates :quantity, numericality: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.establish_connection
+ActiveRecord::Base.connection.tables.each do |table|
+  # MySQL
+  # ActiveRecord::Base.connection.execute("TRUNCATE #{table}") unless table == "schema_migrations"
 
-Category.create([
-  {name: 'Pantry', user_id: 3}, 
-  {name: 'Fridge', user_id: 3}, 
-  {name: 'Books', user_id: 3}, 
-  {name: 'Clothing', user_id: 3}, 
-  {name: 'Hobby', user_id: 3}, 
-  {name: 'Work', user_id: 3}, 
-  {name: 'Transportation', user_id: 3}, 
-  {name: 'Misc.', user_id: 3}
+  # SQLite
+  ActiveRecord::Base.connection.execute("DELETE FROM #{table}") unless table == "schema_migrations"
+  
+
+end
+
+user = User.create! :username => 'John Doe', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+Category.create!([
+  {name: 'Pantry', user_id: user.id}, # id 1
+  {name: 'Fridge', user_id: user.id}, 
+  {name: 'Books', user_id: user.id}, # id 3
+  {name: 'Clothing', user_id: user.id}, 
+  {name: 'Hobby', user_id: user.id}, # id 5
+  {name: 'Work', user_id: user.id},  
+  {name: 'Transportation', user_id: user.id}, # id 6
+  {name: 'Misc.', user_id: user.id} # id 7
 ])
+
+categories = Category.pluck(:id)
+
+200.times do |n|
+  Item.create!(name: Faker::Commerce.product_name, quantity: Faker::Number.between(1,10), category_id: categories.sample, description: Faker::Lorem.paragraph(2))
+end
