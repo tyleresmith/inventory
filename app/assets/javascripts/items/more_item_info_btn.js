b4f73d3a8fa9a49fd9ac4726f666a5af68dc81b5
tyleresmith <oats000@hotmail.com>
2016-08-01 23:06:15 -0400

    refactored js to include model

--- app/assets/javascripts/items/more_item_info_btn.js
+++ app/assets/javascripts/items/more_item_info_btn.js
@@ -3,12 +3,8 @@ $(function() {
   $(".js-more").on("click", function() {
     var id = $(this).data("id");
     $.get("/items/" + id + ".json", function(data) {
-      console.log(data["necessary"]);
-      var inventoryText = "<strong>Unnecessary</strong>";
-      if(data["necessary"]){
-        inventoryText = "<strong>Necessary</strong>";
-      }
-      var descriptionText = "<p>" + data["description"] + "</p><p>" + inventoryText + "</p>";
+      var item = new Item(data);
+      var descriptionText = "<p>" + item.description + "</p>" + item.isNecessary();
       $("#item-" + id).append(descriptionText);
       $("#item-btn-" +id).remove();
     });
