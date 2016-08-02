af0fad71c3e1d6fe8b9c649dd734b8f6c45605a2
tyleresmith <oats000@hotmail.com>
2016-08-01 23:03:22 -0400

    js now only loads on specific page

--- app/assets/javascripts/items/items_index.js
+++ app/assets/javascripts/items/items_index.js
@@ -1,23 +1,29 @@
 
+
+
 $(function() {
-  $.get("/items.json", function(data) {
-  	console.log(data);
-    var necessary = "<strong>Unnecessary</strong>"
-    var itemsHtml = ["<ul>"];
-  	$.each(data, function (index, value) {
-      if(value["necessary"]){
-        necessary = "<strong>Necessary</strong>";
-      }
-      var item = [
-        '<li>',
-          '<h2> '+value["name"]+'('+value["quantity"] +')</h2>',
-          necessary,
-        '<li>'
-      ].join('');
-      itemsHtml.push(item);
-    });
-    itemsHtml.push("</ul>");
-    itemsHtml.join('');
-    $(".item_list").append(itemsHtml);
-  });
+   if ($('body.items.index').length > 0)
+    {
+      $.get("/items.json", function(data) {
+      	console.log(data);
+
+        var itemsHtml = ["<ul>"];
+      	$.each(data, function (index, value) {
+          var necessary = "<strong>Unnecessary</strong>"
+          if(value["necessary"]){
+            necessary = "<strong>Necessary</strong>";
+          }
+          var item = [
+            '<li>',
+              '<h2> '+value["name"]+'('+value["quantity"] +')</h2>',
+              necessary,
+            '<li>'
+          ].join('');
+          itemsHtml.push(item);
+        });
+        itemsHtml.push("</ul>");
+        itemsHtml.join('');
+        $(".item_list").append(itemsHtml);
+      });
+    }
 });
--- app/assets/javascripts/items/items_show.js
+++ app/assets/javascripts/items/items_show.js
@@ -1,14 +1,19 @@
-$(function() {
-  var id = $(".show_item").data("id")
 
-  $.get("/items/"+id+".json", function(data) {
-    var item = new Item(data);
-    var itemHtml = [,
-        '<h1> '+item.name+'</h1>',
-        item.isNecessary(),
-        '<p>Quantity: ' + item.quantity + '</p>',
-        '<p>Description: ' + item.description,
-    ].join('');
-    $(".show_item").append(itemHtml);
-  });
+
+$(function() {
+   if ($('body.items.show').length > 0)
+    {
+      var id = $(".show_item").data("id")
+      $.get("/items/"+id+".json", function(data) {
+        var item = new Item(data);
+        var itemHtml = [,
+            '<h1> '+item.name+'</h1>',
+            item.isNecessary(),
+            '<p>Quantity: ' + item.quantity + '</p>',
+            '<p>Description: ' + item.description,
+        ].join('');
+        $(".show_item").append(itemHtml);
+      });
+    }  
 });
+
