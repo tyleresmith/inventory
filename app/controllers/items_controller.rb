92986711abd94872594b6f258384a0a30f2db7bb
tyleresmith <oats000@hotmail.com>
2016-08-01 20:38:24 -0400

    fixed rendering

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,7 +16,7 @@ class ItemsController < ApplicationController
   def index
     @items = current_user.items
     respond_to do |format|
-      format.html { render :show }
+      format.html { render :index }
       format.json { render json: @items}
     end
   end
